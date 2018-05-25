86bdddf0474e6bdd9888aa04f6a878cfbef1c937
Matthew-Me-yer <39626064+Matthew-Me-yer@users.noreply.github.com>
2018-05-25 09:31:43 -0500

    Create ex11.js

---
+++ ex11.js
@@ -0,0 +1,13 @@
+/*
+Write a function called "isEvenAndGreaterThanTen".
+
+Given a number, "isEvenAndGreaterThanTen" returns whether it is both even and greater than 10.
+*/
+
+function isEvenAndGreaterThanTen(num) {
+  // your code here
+  
+  if(num%2==0  && num>10){
+  return true } else { return false}
+}
+isEvenAndGreaterThanTen(10)
